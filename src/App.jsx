7f9114eef9bdb6167847730d3e6a4424dd076848
shreyansh0009 <saurabhsharraf@gmail.com>
2025-02-11 13:59:21 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useState} from 'react'
 import Login from './components/Auth/Login'
 import Employee from './components/Dashboard/Employee'
 import Admin from './components/Dashboard/Admin'
@@ -10,13 +10,13 @@ function App() {
   //   getLocalStorage()
   // },)
 
+  const [user, setuser] = useState(null)
+
   const handleLogin = (email, passowrd) => {
     if(email == 'admin@me.com' && passowrd == 123) {
-      console.log("welcome admin!");
-      
+      setuser('admin')      
     } else if(email == 'user@me.com' && passowrd == 123) {
-      console.log("welcome user!");
-
+      setuser('employee')
     } else {
       alert("Invalid Credentials!")
       
@@ -24,10 +24,10 @@ function App() {
   }
   
   return (
+
     <div>
-      <Login handleLogin = {handleLogin}/>
-      {/* <Employee /> */}
-      {/* <Admin /> */}
+      {!user ? <Login handleLogin={handleLogin} />: ''}
+      {user == 'admin' ? <Admin /> : <Employee />}
     </div>
   )
 }
