b291d63c9d2d5d74afd3190e3747c785f4161161
shreyansh0009 <saurabhsharraf@gmail.com>
2025-02-12 23:57:54 +0530

    updated

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { createContext } from 'react'
+
+export const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
   return (
     <div>
+      <AuthContext.Provider value={"saurabh"}>
       {children}
+
+      </AuthContext.Provider>
+      
     </div>
   )
 }
