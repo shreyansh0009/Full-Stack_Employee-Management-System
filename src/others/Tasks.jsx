2b1c29b6addbc1f735955ea940ce3c605f5c7977
shreyansh0009 <saurabhsharraf@gmail.com>
2025-02-21 00:49:48 +0530

    updated

--- src/others/Tasks.jsx
+++ src/others/Tasks.jsx
@@ -5,22 +5,22 @@ const Tasks = ({data}) => {
     <div className='flex justify-between gap-4 p-7'>
 
         <div className='h-40 w-1/2 bg-red-500 rounded-xl text-white p-10 cursor-pointer'>
-            <h2 className='text-3xl font-bold'>0</h2>
+            <h2 className='text-3xl font-bold'>{data.taskNumber.newTask}</h2>
             <h3 className='text-2xl font-semibold'>New Task</h3>
         </div>
 
         <div className='w-1/2 bg-blue-500 rounded-xl text-white p-10 cursor-pointer'>
-            <h2 className='text-3xl font-bold'>3</h2>
+            <h2 className='text-3xl font-bold'>{data.taskNumber.completed}</h2>
             <h3 className='text-2xl font-semibold'>Completed</h3>
         </div>
 
         <div className='h-40 w-1/2 bg-green-500 rounded-xl text-white p-10 cursor-pointer'>
-            <h2 className='text-3xl font-bold'>8</h2>
-            <h3 className='text-2xl font-semibold'>Accepted</h3>
+            <h2 className='text-3xl font-bold'>{data.taskNumber.active}</h2>
+            <h3 className='text-2xl font-semibold'>Active</h3>
         </div>
 
         <div className='h-40 w-1/2 bg-yellow-500 rounded-xl text-white p-10 cursor-pointer'>
-            <h2 className='text-3xl font-bold'>1</h2>
+            <h2 className='text-3xl font-bold'>{data.taskNumber.failed}</h2>
             <h3 className='text-2xl font-semibold'>Failed</h3>
         </div>
       
