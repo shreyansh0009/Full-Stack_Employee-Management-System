857f112eee63be95d1123a0c9e2b571bd900e70c
shreyansh0009 <saurabhsharraf@gmail.com>
2025-03-27 10:49:19 +0530

    updated

--- src/components/Tasklist/TaskList.jsx
+++ src/components/Tasklist/TaskList.jsx
@@ -4,32 +4,32 @@ import New from './New'
 import Complete from './Complete'
 import Failed from './Failed'
 
-const TaskList = ({data}) => {
+const TaskList = ({data}) => {    
   return (
     <div id='tasklist' className='h-[42%] m-7 pb-4 flex items-center justify-start gap-5 flex-nowrap overflow-x-auto'>
-
-        <Accept />
+        {/* <Accept />
         <New />
         <Complete />
-        <Failed />
-        
-        {/* <div className='h-full w-[30%] rounded-xl shrink-0 bg-violet-400'>
+        <Failed /> */}
 
-        <div className='flex justify-between p-5'>
-                <h3 className='font-semibold bg-red-600 p-1 px-5 rounded-md'>High</h3>
-                <h3 className='font-semibold'>31 Jan 2025</h3>
-            </div>
+        {data.tasks.map((e) => {
+
+            if(e.active) {
+                return <Accept />
+            }
             
-            <div className='px-5'>
-            <h1 className='font-bold text-3xl mb-3'>
-                Make YouTube Video!
-            </h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nam? Numquam ex voluptas delectus architecto.
-            </p>
-            </div>
+            if(e.newTask) {
+                return <New />
+            }
 
-        </div> */}
+            if(e.completed) {
+                return <Complete />
+            }
+
+            if(e.failed) {
+                return <Failed />
+            }
+        })}
       
     </div>
   )
